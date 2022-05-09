7471cd05e605c6d2d7121a75ba3af2810f6720a2
iversonfir <iversonfir@gmail.com>
2022-05-10 07:34:16 +0800

    3.4 批次查詢子網頁內容V2

--- scraper.py
+++ scraper.py
@@ -1,20 +1,17 @@
 import requests as req
 from bs4 import BeautifulSoup as bs
 
-
 resp=req.get('https://www.bbc.com/zhongwen/trad/topics/c83plve5vmjt')
-
 soup=bs(resp.text,'lxml')
 urls=soup.find_all('a',{'class':'qa-story-image-link'})
 tag_list=[]
 for url in urls:
     context='https://www.bbc.com'
-    sub_resp=req.get(context+url.get('href'))
+    url=context+url.get('href')
+    sub_resp=req.get(url)
     sub_soup=bs(sub_resp.text,'lxml')
-    tags=sub_soup.find_all('li',{'class','bbc-1msyfg1 e1hq59l0'})
-    for tag  in tags:
-        print(tag.find('a').getText())
+    tags=sub_soup.find_all('li',{'class':'bbc-1msyfg1 e1hq59l0'})
+    for tag in tags:
         tag_list.append(tag.find('a').getText())
-
 print(tag_list)
 
